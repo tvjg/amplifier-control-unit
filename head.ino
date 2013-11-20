279d81854fc9005205b9525d31554f5ecbe49a23
John Guidry <lowdowndirtyrottenscoundrel@gmail.com>
2013-11-20 00:47:59 -0600

    Add optional defines to toggle serial debugging

--- head.ino
+++ head.ino
@@ -6,6 +6,9 @@
 
 #include "head.h"
 
+#define SERIAL_LOG
+#define SERIAL_RAW_DUMP 
+
 #define ext_int_on(interrupt)  { EIMSK |=  (1 << interrupt); }
 #define ext_int_off(interrupt) { EIMSK &=  ~(1 << interrupt); }
 
@@ -33,11 +36,13 @@ static uint8_t ir_commands[NUMBER_OF_IR_CODES][IR_RAW_SIZE];
 int main (void) {
   init();  // take care of Arduino timers etc. etc.
 
+#ifdef SERIAL_LOG
   Serial.begin(57600);
 
   // F() macro neatly wraps string storage/retrieval from flash memory
   // http://playground.arduino.cc/Main/Printf#sourceblock5
   Serial.println("Ready to decode IR!");
+#endif
 
   DDRB  |=  (1 << PB5);    // PB5/D13/LED as output
 
@@ -55,17 +60,25 @@ int main (void) {
       ir_command_t command = match_ir_code(ir_signal_copy);
       const char* msg = (command < 0) ? "Unknown Code" : command_labels[command];
 
+#ifdef SERIAL_LOG
+#ifdef SERIAL_RAW_DUMP
       Serial.println("\n\rReceived: \n\rOFF\t\tON");
-      for (uint8_t i = 0; i < change_count; i+=2) {
+      uint8_t i = 0;
+      for (i; i < IR_RAW_SIZE; i+=2) {
         Serial.print(ir_signal_copy[i], DEC);
         Serial.print("\t\t");
         Serial.println(ir_signal_copy[i+1], DEC);
+        
+        if (ir_signal_copy[i+1] == 0xFF) break; 
       }
 
       Serial.print("\n\rPulse Count: ");
-      Serial.println(change_count+1);
-      Serial.print("Diagnosis: ");
+      Serial.println(i+2);
+#endif
+
+      Serial.print("\n\rDiagnosis: ");
       Serial.println(msg);
+#endif
     }
   }
 }
